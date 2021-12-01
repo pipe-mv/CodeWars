27c60abb99057253d55dfde6a456818b8c801e96
Felipe Marín Vásquez <pipemv@felipes-mbp.home>
2021-12-02 01:54:49 +1100

    Cambio1

--- CodeWars_Kyu_7.js
+++ CodeWars_Kyu_7.js
@@ -1,1 +1,10 @@
-sgfbvsfdbvsdxfvs
+function solution(nums){
+  return (nums === [] || nums === null || nums === "nil") ? [] : nums.sort(function (a , b) {return a - b})
+}
+
+console.log(solution([1,2,3,10,5]));
+console.log(solution(null));
+console.log(solution("nil"));
+console.log(solution([]));
+console.log(solution([20, 2, 10]));
+console.log(solution([2, 20, 10]));
